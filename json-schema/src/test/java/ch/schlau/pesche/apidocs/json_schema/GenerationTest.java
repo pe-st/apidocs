f4e65e8526ae1e33c93dd966702ba0227d5875c5
Peter Steiner <unistein+n38151@gmail.com>
2019-03-07 14:18:27 +0100

    Add initial json-schema example

---
+++ json-schema/src/test/java/ch/schlau/pesche/apidocs/json_schema/GenerationTest.java
@@ -0,0 +1,33 @@
+package ch.schlau.pesche.apidocs.json_schema;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ch.schlau.pesche.apidocs.json_schema.generated.PinCheckRequest;
+
+class GenerationTest {
+
+    @Test
+    void json_roundtrip() throws IOException {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        PinCheckRequest request = new PinCheckRequest();
+        request.setUuid("abcd");
+        request.setPinblock("secret");
+
+        String jsonString = mapper.writeValueAsString(request);
+
+        assertThat(jsonString, is("{\"uuid\":\"abcd\",\"pinblock\":\"secret\"}"));
+
+        PinCheckRequest roundtrip = mapper.readerFor(PinCheckRequest.class).readValue(jsonString);
+        assertThat(roundtrip.getUuid(), is("abcd"));
+    }
+
+}
