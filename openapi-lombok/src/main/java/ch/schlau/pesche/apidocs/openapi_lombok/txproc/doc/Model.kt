793f42e11c84cb3828c386cd3ad09336f655d15f
Peter Steiner <unistein+n38151@gmail.com>
2019-03-26 17:55:19 +0100

    Try Kotlin for its multiline literals
    (Java still hasn't got them)

---
+++ openapi-lombok/src/main/java/ch/schlau/pesche/apidocs/openapi_lombok/txproc/doc/Model.kt
@@ -0,0 +1,17 @@
+package ch.schlau.pesche.apidocs.openapi_lombok.txproc.doc
+
+/**
+ * Centralized strings for use with [org.eclipse.microprofile.openapi.annotations.media.Schema]
+ * annotations
+ */
+object Model {
+
+    const val UUID = "Unique ID of the request"
+
+    const val PIN_BLOCK = """
+Encrypted binary data containing a PIN
+
+Fieldcode: C003
+"""
+
+}
