11969d08b4da7e557458b020caebe52e8ab01c21
Peter Steiner <unistein+mac-grosapi@gmail.com>
2019-08-01 10:54:54 +0200

    Concentrate on code-first and design-first

--- spring-restdocs/src/main/java/ch/schlau/pesche/apidocs/spring_restdocs/rest/JaxrsApplication.java
+++
@@ -1,8 +0,0 @@
-package ch.schlau.pesche.apidocs.spring_restdocs.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class JaxrsApplication extends Application {
-}
