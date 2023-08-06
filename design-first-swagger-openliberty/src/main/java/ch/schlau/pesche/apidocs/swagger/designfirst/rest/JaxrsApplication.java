3f4ab5c2f9d6b9fea64bac943f4197246a552767
Peter Steiner <unistein+pitchounette@gmail.com>
2023-08-06 09:14:57 +0200

    Remove OpenLiberty examples
    I don't have the time to keep them up to date

--- design-first-swagger-openliberty/src/main/java/ch/schlau/pesche/apidocs/swagger/designfirst/rest/JaxrsApplication.java
+++
@@ -1,8 +0,0 @@
-package ch.schlau.pesche.apidocs.swagger.designfirst.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class JaxrsApplication extends Application {
-}
